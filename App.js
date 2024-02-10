8c02e6b61b759fd4c062259aded9e4faea87f7fe
Alfredo Dominguez <thecatura.ad@gmail.com>
2024-02-10 16:33:44 -0400

    map-view testing

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-// import MapboxGL from "@rnmapbox/maps";
+import MapboxGL from "@rnmapbox/maps";
 
-// MapboxGL.setAccessToken("sk.eyJ1IjoiZGZyZWR1ZGU0OCIsImEiOiJjbHNnaDY5YTIxbXo4Mmpxb3ZoczNid3duIn0.D7YgYcus56IQND7PT_Mx_Q");
+MapboxGL.setAccessToken("sk.eyJ1IjoiZGZyZWR1ZGU0OCIsImEiOiJjbHNnaDY5YTIxbXo4Mmpxb3ZoczNid3duIn0.D7YgYcus56IQND7PT_Mx_Q");
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      {/* <MapboxGL.MapView style={styles.map} /> */}
+      <MapboxGL.MapView style={styles.map} />
       {/* <StatusBar style="auto" /> */}
     </View>
   );
