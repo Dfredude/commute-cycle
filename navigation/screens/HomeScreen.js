b94dabb37cae5d805fd1861de0d5b99b7285711c
Kanishk Dogra <Kanishmain@gmail.com>
2024-02-11 11:04:19 -0500

    button

--- navigation/screens/HomeScreen.js
+++ navigation/screens/HomeScreen.js
@@ -1,19 +1,28 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { SearchBar } from 'react-native-elements';
+import { View, Text,Button, } from 'react-native';
 import {StyleSheet} from 'react-native';
 import Mapbox from '@rnmapbox/maps';
 import Map from '../../components/Map';
+import {StopwatchApp} from './StopwatchApp'
 
 export default function HomeScreen({ navigation }) {
-  // const Map = new Mapbox.MapView({
-  //   styleURL: Mapbox.StyleURL.Street,
-  //   zoomEnabled: true,
-  //   rotateEnabled: true,
-  // });
+
+  const [stopwatchStarted, setStopwatchStarted] = React.useState(false);
+  const handleStartStopwatch = (res) => {
+    if(res){
+      setStopwatchStarted(!stopwatchStarted);
+    }
+    
+  };
     return (
       <View style={styles.page}>
         <View style={styles.container}>
           <Map></Map>
+          <View style={styles.buttonContainer}>
+          <Button
+          title={stopwatchStarted ? 'Stop' : 'Start'} onPress={() =>{  return handleStartStopwatch(true), <StopwatchApp></StopwatchApp>}}/>
+          </View>
         </View>
       </View>
     );
@@ -27,5 +36,10 @@ const styles = StyleSheet.create({
   container: {
     height: '100%',
     width: '100%',
-  }
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 16, // Adjust bottom spacing as needed
+    right: 16, // Adjust right spacing as needed
+  },
 });
